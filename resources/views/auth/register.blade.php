44dc972526b2705d1acc8783d615935eb74c4161
Tajveez <Tajveez786@gmail.com>
2021-01-04 23:28:09 +0500

    Register controller and view added

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-6/12 bg-gray-100 p-6 rounded-lg">
+            Register
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/', function () {
     return view('posts.index');
