e61f32a77a9f709166b0db5f05a71a1e685b24a0
Tajveez <Tajveez786@gmail.com>
2021-01-07 23:36:38 +0500

    More validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,17 +16,32 @@
 
                 <div class="mb-4">
                     <label for="username" class="sr-only">Username</label>
-                    <input type="text" id="username" placeholder="Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                    <input type="text" id="username" placeholder="Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror">
+                    @error('username')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="text" id="email" placeholder="Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                    <input type="text" id="email" placeholder="Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg  @error('email') border-red-500 @enderror">
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="password" class="sr-only">Password</label>
-                    <input type="password" id="password" placeholder="Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                    <input type="password" id="password" placeholder="Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror">
+                    @error('password')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 
                 <div class="mb-4">
