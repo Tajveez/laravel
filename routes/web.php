cf275b3311bfc6fdfbdb613d8a328d8b5d7c4e76
Tajveez <Tajveez786@gmail.com>
2021-01-09 22:45:13 +0500

    Registration done

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dashboard', [DashboardController::class, 'Index'])->name('dashboard');
 Route::get('/register', [RegisterController::class, 'Index'])->name('register');
 Route::post('/register', [RegisterController::class, 'Store']);
 
