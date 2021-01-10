e76bae1892d03000335cc7062b92d33b4b6fb938
Tajveez <Tajveez786@gmail.com>
2021-01-10 23:29:36 +0500

    Login controller route view made

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', [DashboardController::class, 'Index'])->name('dashboard');
+Route::get('/login', [LoginController::class, 'Index'])->name('login');
 Route::get('/register', [RegisterController::class, 'Index'])->name('register');
 Route::post('/register', [RegisterController::class, 'Store']);
 
