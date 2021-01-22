293881d8a437f420d575f6217e6058f4d9c926ad
Tajveez <Tajveez786@gmail.com>
2021-01-22 23:35:44 +0500

    Delete Post functionality

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -24,16 +24,22 @@
                     <div class="mb-4">
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
-
+                        <div>
+                            <form action="{{ route('posts.destroy', $post) }}" method="POST" class="mr-1">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-red-500">Delete</button>
+                                </form>
+                        </div>
                         <div class="flex items-center">
                             @auth
                                 @if(!$post->likedBy(auth()->user()))
-                                <form action="{{ route('posts.likes', $post->id) }}" method="POST" class="mr-1">
+                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
                                     @csrf
                                     <button type="submit" class="text-blue-500">Like</button>
                                 </form> 
                                 @else
-                                <form action="{{ route('posts.likes', $post->id) }}" method="POST" class="mr-1">
+                                <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="text-red-500">Unlike</button>
